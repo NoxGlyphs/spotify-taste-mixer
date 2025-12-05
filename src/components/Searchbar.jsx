2afd34840d82d7fdee36b9eb88e385d817e976da
NoxGlyphs <jdonosocam@gmail.com>
2025-12-05 20:11:07 +0100

    searchbar skeleton

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Searchbar({ onSearch }) {
+    const [query, setQuery] = useState("")
+    const [isActive, setIsActive] = useState(false)
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        onSearch(query);
+    }
+
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="searchInput"
+                onFocus={() => setIsActive(true)}
+                onBlur={() => setIsActive(false)}
+                placeholder="Search for artists, songs, or albums"
+                required
+            />
+            <button type="submit">Search</button>
+        </form>
+    );
+}
